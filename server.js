f5235f3ca8fbbeecc6f719c49c5bcd0f4e02877f
Samarth24558 <samarth24558@gmail.com>
2026-08-13 20:18:51 +0530

    Solved corn to cron

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import {sql} from "./config/db.js"
 import rateLimiter from "./middleware/rateLimit.js";
 import transactionsRoutes from "./routes/transactionsRoutes.js"
-import job from "./config/corn.js"
+import job from "./config/cron.js"
 
 dotenv.config()
 
